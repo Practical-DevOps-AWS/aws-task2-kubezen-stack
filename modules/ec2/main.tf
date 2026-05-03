25f36e0caa7bc86e72c80b9005eeb6e0b5349697
Maksym Romanyuk <maksym23102006@gmail.com>
2026-05-04 01:43:08 +0300

    fix: some changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "my_Amazon_Linux" {
   ami                    = data.aws_ami.latest_amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [var.vpc_security_group_ids.id]
 
   user_data = file("./modules/ec2/install_apache.sh")
 
--- modules/sg/outputs.tf
+++ modules/sg/outputs.tf
@@ -1,3 +1,3 @@
 output "vpc_security_group_ids" {
-  value = [aws_security_group.SecurityGroup_EC2inPublicSubnet.id]
+  value = aws_security_group.SecurityGroup_EC2inPublicSubnet
 }
